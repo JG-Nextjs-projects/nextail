9c6bfa6037a9343badf49228918dd6c62e88fe67
Jose Guerra <jose.guerra@dodsgroup.com>
2022-02-02 15:09:25 +0000

    added interface props

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -6,9 +6,23 @@ import React from 'react';
 
 const root = process.cwd();
 
+interface Post {
+  slug: string;
+  meta: {
+    title: string;
+    description: string;
+    date: number;
+    author: string;
+  };
+}
+
+interface PostProps extends Post {
+  posts: Post[];
+}
+
 export const getStaticProps: GetStaticProps = async (context) => {
   const files = fs.readdirSync(path.join(root, 'src/pages/posts'));
-  const posts = [];
+  const posts: Post[] = [];
 
   files.map(async (file) => {
     const slug = file.replace('.mdx', '');
@@ -24,13 +38,20 @@ export const getStaticProps: GetStaticProps = async (context) => {
   };
 };
 
-const Posts = ({ posts }) => {
+const Posts = ({ posts }: PostProps) => {
+  console.log(posts);
   return (
     <div>
       <h2>Hello from blog</h2>
-      {posts.map((post) => (
-        <li>{post.meta.title}</li>
-      ))}
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link href={`/posts/${post.slug}`}>
+              <a>{post.meta.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
