50e7d97f471d715617ec3244f979a4f277cc09c8
Jose Guerra <jose.guerra@dodsgroup.com>
2022-02-11 15:54:40 +0000

    updated awaits

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -6,61 +6,47 @@ import React from 'react';
 
 const root = process.cwd();
 
-interface Post {
-  slug: string;
-  meta?: {
-    title: string;
-    description: string;
-    date: number;
-    author: string;
-  };
-}
-
 interface Meta {
   title: string;
   description: string;
   date: number;
   author: string;
+  slug: string;
 }
 
-interface PostProps extends Post {
-  posts: Post[];
+interface PostProps extends Meta {
+  posts: Meta[];
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const files = fs.readdirSync(path.join(root, 'src/pages/posts'));
-  // const posts: Post[] = [];
 
-  const posts = files.map(async (file) => {
+  const promises = files.map(async (file) => {
     const { meta } = await import(`./posts/${file}`);
 
-    console.log(meta);
-    return {
-      meta,
-    };
+    return meta;
   });
 
-  // console.log(posts);
+  const posts = await Promise.all(promises);
 
   return {
-    props: {
-      posts,
-    },
+    props: { posts },
   };
 };
 
 const Posts = ({ posts }: PostProps) => {
+  console.log(posts);
   return (
     <div>
       <h2>Hello from blog</h2>
       <ul>
-        {/* {posts.map((post) => (
+        {posts.map((post) => (
           <li key={post.slug}>
             <Link href={`/posts/${post.slug}`}>
-              <a>{post.slug}</a>
+              <a>{post.title}</a>
             </Link>
           </li>
-        ))} */}
+        ))}
       </ul>
     </div>
   );
