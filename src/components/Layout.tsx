8f62d8a158316f9b91e109db8004969a3683c2be
jsguerra <jose@gcostudios.com>
2022-02-12 21:29:16 +0000

    updated layout structure

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const Layout = () => {
-  return <div></div>;
+import Footer from './Footer';
+import Header from './Header';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
