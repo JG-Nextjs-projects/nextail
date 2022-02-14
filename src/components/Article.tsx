0a8369cc80d038877ed32fc5200511c3f872cf43
jsguerra <jose@gcostudios.com>
2022-02-14 09:37:05 +0000

    setup initial article structure

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const Article = () => {
-  return <article>Article</article>;
+interface ArticleProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const Article: React.FC<ArticleProps> = ({ children, title }) => {
+  return (
+    <article>
+      <header>
+        <h1>{title}</h1>
+      </header>
+      {children}
+    </article>
+  );
 };
 
 export default Article;
