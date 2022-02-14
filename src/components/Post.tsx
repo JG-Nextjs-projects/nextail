25236daf601d663f7594f1df9358c7bff9b3b016
jsguerra <jose@gcostudios.com>
2022-02-14 09:37:18 +0000

    setup basic post page structure

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Post = () => {
-  return <div>Post</div>;
+import Article from './Article';
+
+interface ArticleProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const Post: React.FC<ArticleProps> = ({ children, title }) => {
+  return (
+    <div className="mb-16">
+      <Article title={title}>{children}</Article>
+    </div>
+  );
 };
 
 export default Post;
