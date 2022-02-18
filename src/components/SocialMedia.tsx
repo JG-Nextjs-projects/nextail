3c967b6d263fe66dfa385e5c1dbe3ed79b8bdcb1
jsguerra <jose@gcostudios.com>
2022-02-18 10:13:22 +0000

    updated component

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import Linkedin from './icons/linkedin.svg';
+
 const SocialMedia = () => {
-  return <div>SocialMedia</div>;
+  const components = {
+    linkedin: Linkedin,
+  };
+
+  console.log(components.linkedin);
+
+  return (
+    <ul>
+      <li></li>
+    </ul>
+  );
 };
 
 export default SocialMedia;
